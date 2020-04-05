fdbf7ef2afcd7fff6e1739663eec1c69d653e348
Ray Alva <rayalva407@gmail.com>
2020-04-05 04:29:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiou]/i)
+    true
+  elsif word.match(/\A[aeiou]/i) == nil
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  
 end
 
 def words_five_letters_long(text)
